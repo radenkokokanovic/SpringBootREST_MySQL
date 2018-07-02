60afaf315f8612ff35d1b7d6cd2f09e6f888f5ae
Radenko Kokanovic <radenko.kokanovic@gmail.com>
2018-07-02 13:45:12 +0200

    Second commit add sql for database and tables

---
+++ SpringBoot-Mysql/src/main/resources/test.sql
@@ -0,0 +1,351 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2018 at 01:44 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `address`
+--
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `street_name` varchar(40) DEFAULT NULL,
+  `city_name` varchar(40) DEFAULT NULL,
+  `state_name` varchar(40) DEFAULT NULL,
+  `zipcode` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `address`
+--
+
+INSERT INTO `address` (`id`, `street_name`, `city_name`, `state_name`, `zipcode`) VALUES
+(1, 'Kondapur', 'Hyderabad', 'AP', '532'),
+(2, 'Kondapur', 'Hyderabad', 'AP', '532'),
+(3, 'Kondapur', 'Hyderabad', 'AP', '532');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  `address` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`id`, `first_name`, `last_name`, `salary`, `address`) VALUES
+(1, 'Manoj', 'Kumar', 5000, 1),
+(3, 'Manoj', 'Kumar', 5000, 2),
+(5, 'Manoj', 'Kumar', 5000, 3),
+(6, 'Dilip', 'Kumar', 3000, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hibernate_sequence`
+--
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES
+(17);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `korisnik`
+--
+
+CREATE TABLE `korisnik` (
+  `id` int(11) NOT NULL,
+  `Adresa` varchar(255) DEFAULT NULL,
+  `godine` int(11) NOT NULL,
+  `ime` varchar(255) DEFAULT NULL,
+  `nadimak` varchar(255) DEFAULT NULL,
+  `prezime` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `korisnik`
+--
+
+INSERT INTO `korisnik` (`id`, `Adresa`, `godine`, `ime`, `nadimak`, `prezime`) VALUES
+(0, 'New Delhi', 43, 'David', 'Beton', 'Readin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `laptop`
+--
+
+CREATE TABLE `laptop` (
+  `lid` int(11) NOT NULL,
+  `lname` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `laptop`
+--
+
+INSERT INTO `laptop` (`lid`, `lname`) VALUES
+(101, 'Dell');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `persons`
+--
+
+CREATE TABLE `persons` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(50) NOT NULL,
+  `prezime` varchar(80) NOT NULL,
+  `adresa` varchar(80) NOT NULL,
+  `godine` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `persons`
+--
+
+INSERT INTO `persons` (`id`, `ime`, `prezime`, `adresa`, `godine`) VALUES
+(1, 'Radenko', 'Kokanovic', 'Crnjelovo', 40),
+(2, 'Radenko2', 'Kokanovic', 'Crnjelovo', 25),
+(5, 'Slavko', 'Kokanovic', 'Crnjelovo', 25),
+(6, 'TEST', 'TEST', 'TEST', 30),
+(7, 'John', 'Lukas', 'LA', 30),
+(8, 'Simo', 'Lukas', 'LA', 30),
+(9, 'Petar', 'Vrbas', 'WA', 40),
+(10, 'Milenko', 'Jovanovic', 'Bijeljina', 40),
+(11, 'Danijel', 'Kovacevic', 'Beograd', 27),
+(12, 'Navin', 'Readin', 'New Delhi', 43),
+(13, 'Navin', 'Readin', 'New Delhi', 43),
+(14, 'Navin', 'Readin', 'New Delhi', 43),
+(15, 'David', 'Readin', 'New Delhi', 43);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `podaci`
+--
+
+CREATE TABLE `podaci` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(50) NOT NULL,
+  `prezime` varchar(100) NOT NULL,
+  `adresa` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `podaci`
+--
+
+INSERT INTO `podaci` (`id`, `ime`, `prezime`, `adresa`) VALUES
+(1, 'radenko', 'kokanovic', 'Crnjelovo\r\n'),
+(2, 'RadenkoTest', 'KokanovicTest', 'Gornje Crnjelovo'),
+(3, 'RadenkoTest', 'KokanovicTest', 'Gornje Crnjelovo'),
+(16, 'Philips', 'Jovanovic', 'Beograd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` bigint(10) NOT NULL,
+  `student_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `student_name`) VALUES
+(6, 'Radenko'),
+(7, 'Slavko'),
+(8, 'Milenko');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_detail`
+--
+
+CREATE TABLE `student_detail` (
+  `student_id` bigint(10) NOT NULL,
+  `student_mobile_number` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student_detail`
+--
+
+INSERT INTO `student_detail` (`student_id`, `student_mobile_number`) VALUES
+(6, '065353917'),
+(7, '065353917'),
+(8, '065353917');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `name`) VALUES
+(1, 'test', 'Radenko'),
+(2, 'test', 'test'),
+(3, 'jpa', 'spring');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `address`
+--
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `korisnik`
+--
+ALTER TABLE `korisnik`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `laptop`
+--
+ALTER TABLE `laptop`
+  ADD PRIMARY KEY (`lid`);
+
+--
+-- Indexes for table `persons`
+--
+ALTER TABLE `persons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `podaci`
+--
+ALTER TABLE `podaci`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `student_detail`
+--
+ALTER TABLE `student_detail`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `address`
+--
+ALTER TABLE `address`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `persons`
+--
+ALTER TABLE `persons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `podaci`
+--
+ALTER TABLE `podaci`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `student_detail`
+--
+ALTER TABLE `student_detail`
+  ADD CONSTRAINT `student_detail_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `student` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
